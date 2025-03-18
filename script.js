5b09bba2f1a3eb9aea310b23cdc8e4a63cb28d42
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2025-03-17 22:17:23 -0300

    js

---
+++ script.js
@@ -0,0 +1,23 @@
+//log
+console.log ("Exemplo JS");
+
+//variaveis
+var primeirotipo;
+let segundotipo;
+const terceirotipo = 1;
+
+let nome = "JOÃO MIGUEL";
+let idade = "21";
+
+console.log(nome);
+console.log(idade);
+
+nome = idade;
+
+console.log(nome);
+
+function teamomariaeduarda(){
+    document.getElementById("texto").textContent = "Te amo muito Maria Eduarda!";
+    document.getElementById("texto").style.color = "pink";
+
+}
